d01683eed7c5dac0a066157685c73c4df47a3c92
Marththt <alessio.martinelli1@student.univaq.it>
2023-05-26 16:07:04 +0200

    roba
    silvia dorme

---
+++ main.php
@@ -0,0 +1,6 @@
+<?php
+require_once ("C:/users/Utente/Desktop/Agor/Agora/Control/CManagePost");
+require_once ("C:/Users/Utente/Desktop/Agor/Agora/Entity/EPost.php");
+
+$p = new CManagePost();
+$p->newSketch();
